0e160bfa19efc6af82420292560b32fef8126105
wala-calendar <kentaro.sakka@gmail.com>
2025-04-14 14:31:18 +0900

    Add 31 background images for daily calendar

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
-// PLACEHOLDER
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('wala-calendar').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
